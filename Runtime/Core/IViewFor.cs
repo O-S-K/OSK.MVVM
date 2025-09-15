806120f42c2d1bf0d6624b444d758a1ce0eda3fb
Osk <52168556+O-S-K@users.noreply.github.com>
2025-09-15 18:42:23 +0700

    Update Mvvm

---
+++ Runtime/Core/IViewFor.cs
@@ -0,0 +1,10 @@
+﻿namespace OSK.MVVM
+{
+    public interface IViewFor 
+    {
+        void SetViewModel(ViewModelBase vm);
+        void OnOpen();
+        void OnClose();
+        TransitionType Transition { get; }
+    }
+}
